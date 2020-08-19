a1dc02c2edd85efb842f1310c6ba2a97663d97f2
Allan Gongora <agongora4030@gmail.com>
2020-08-19 10:54:03 -0700

    predict works

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from .models import DB, User
-from .twitter import insert_example_users
+from .twitter import insert_example_users, add_update_user
+from .predict import predict_user
 
 
 def create_app():
@@ -11,15 +12,42 @@ def create_app():
 
     @app.route('/')
     def root():
-        return render_template('base.html', title='Home',
-                               users=User.query.all())
+        return render_template('base.html', title='Home', users=User.query.all())
+
+    @app.route('/user', methods=['POST'])
+    @app.route('/user/<name>', methods=['GET'])
+    def user(name=None, message=''):
+        name = name or request.values['user_name']
+        try:
+            if request.method == 'POST':
+                add_update_user(name)
+                message = "User {} successfully added!".format(name)
+            tweets = User.query.filter(User.name == name).one().tweets
+        except Exception as e:
+            message = "Error adding {}: {}".format(name, e)
+            tweets = []
+        return render_template('user.html', title=name, tweets=tweets,
+                               message=message)
+
+    @app.route('/compare', methods=['POST'])
+    def compare(message=''):
+        user1, user2 = sorted([request.values['user1'],
+                               request.values['user2']])
+        if user1 == user2:
+            message = 'Cannot compare a user to themselves!'
+        else:
+            prediction = predict_user(user1, user2,
+                                      request.values['tweet_text'])
+            message = '"{}" is more likely to be said by {} than {}'.format(
+                request.values['tweet_text'], user1 if prediction else user2,
+                user2 if prediction else user1)
+        return render_template('prediction.html', title='Prediction', message=message)
 
     @app.route('/update')
     def update():
         # Reset the database
         insert_example_users()
-        return render_template('base.html', title='Users updated!',
-                               users=User.query.all())
+        return render_template('base.html', title='Users updated!', users=User.query.all())
 
     @app.route('/reset')
     def reset():
